3c0273193809d181667099d8aea80ec2e4145a30
PorkyJames <kwanjko17@gmail.com>
2024-04-23 17:10:26 -0700

    Added first / route

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { PORT } from "./config.js";
 
-
+//! Basic set up for Express Backend
 const app = express();
 
 app.listen(PORT, () => {
     console.log(`App is listening to port : ${PORT}`);
 })
+
+app.get("/", (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to MERN Stack Tutorial')
+});
+
