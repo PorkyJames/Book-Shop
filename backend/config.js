8444fb60133c7e5745cbb3607fac984f744516a0
PorkyJames <kwanjko17@gmail.com>
2024-04-23 17:25:06 -0700

    Connected MongoDB / Mongoose to App

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = `mongodb+srv://kwanjko17:qBSS1qeklGj3I9Sp@book-shop.ymgwjbb.mongodb.net/?retryWrites=true&w=majority&appName=Book-Shop`
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,28 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 //! Basic set up for Express Backend
-const app = express();
+//? Includes initializing that the app will use express
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port : ${PORT}`);
-})
+//! Connecting MongoDB / Mongoose db to the app
+//? With express, you can 
+
+const app = express();
 
 app.get("/", (request, response) => {
     console.log(request)
-    return response.status(234).send('Welcome to MERN Stack Tutorial')
+    return response.status(201).send('Welcome to MERN Stack Tutorial')
 });
 
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to Database')
+        app.listen(PORT, () => {
+            console.log(`App is listening to port : ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
