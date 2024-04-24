b3f91aafd94dcc9e78d935e7fdfc08b656218f35
PorkyJames <kwanjko17@gmail.com>
2024-04-23 17:31:46 -0700

    Created Book Model

---
+++ backend/models/book.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        yearPublished: {
+            type: Number,
+            required: true,
+        },
+        summary: {
+            type: String,
+            required: true,
+        }
+    }
+    {
+        timestamp: true,
+    }
+)
+
+//! Establish that the book variable will be a model that has a name property which
+//! identifies as a string.
+//? Note, that after the "Book" name of the model, you can create the schema that way, but you can easily 
+//? change it to a variable prior and just replace it with the variable that has a schema. 
+
+//* Old
+// export const Book = mongoose.model('Book', { name: String })
+
+//* New
+export const Book = mongoose.model('Book', bookSchema)
